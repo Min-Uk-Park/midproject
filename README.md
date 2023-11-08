50c04844464b462c4c1b396ad15cfa65c319a489
ParkMinUk <parkmxxuk@gmail.com>
2023-11-08 18:00:02 +0900

    api request and output

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# midproject
+# opensource development project(2_2)
+*********
+## midproject
+#### 서울시 버스위치정보조회서비스 (첨부파일 참조)를 활용하여 간단한 애플리케이션을 개발하는 프로젝트입니다. 
+**********
+## Development tool
+### 1. vs code
+
+### 2. streamlit
+****
+## Development language
+### python
+
+
---
+++ midproject.py
@@ -0,0 +1,75 @@
+import sys,os
+# streamlit run midproject.py # 웹 실행 위한 명령프롬포트 입력문장 100100118
+# print(os.getcwd())
+# c:\lang_practice\Python_practice
+
+sys.path.append('c:/Users/LG/anaconda3/Lib/site-packages')
+import requests,re
+from bs4 import BeautifulSoup as bs 
+import streamlit as st
+import time
+
+sys.path.append('c:\lang_practice\Python_practice')
+url = 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt'
+
+servicekey = '0OhBU7ZCGIobDVKDeBJDpmDRqK3IRNF6jlf/JB2diFAf/fR2czYO9A4UTGcsOwppV6W2HVUeho/FPwXoL6DwqA=='
+busrouteid = ''
+trynum = 1
+
+
+# 값 입력받기
+busrouteid = st.text_input('please write busRouteId', key = trynum)
+# clicked = st.button('click button') # 버튼은 따로 만들지 않는다. press button하면 값이 자동으로 입력되기 때문
+trynum += 1
+
+# if 정보를 입력하기 전이라면 ~ 
+if (busrouteid == "") :
+    st.write('please write data and press or click the enter')
+
+# if 정보를 입력하고 해당 정보가 옳은 정보라면 ~ 
+elif (re.match('\d{9}',busrouteid)) :
+    
+    # 입력 정보가 맞다면 'Success' 문구를 띄운다!
+    with st.spinner('Wait for it'):
+        time.sleep(5)
+    
+    st.success("Success")
+    
+    params ={'serviceKey' : servicekey, 'busRouteId' : busrouteid, 'startOrd' : '1', 'endOrd' : '10'}
+
+    response = requests.get(url, params=params)
+
+
+    soup = bs(response.text,'lxml-xml')
+    st.title('서울시 버스위치 정보 조회')
+
+
+
+    # write 즉, 출력내용 구하기 : 버스 번호, 좌표값
+
+    x = soup.find('tmX').text
+    y = soup.find('tmY').text
+    busnum = soup.find('plainNo').text
+
+    data_key = ['busnum', 'x', 'y']
+    data_list = [busnum, x, y]
+    data_dict = {}
+
+
+    for i, data in enumerate(data_list) : 
+        data_dict[f'{data_key[i]}'] = data
+        
+    
+    st.write('bus number : ' + busnum)
+    st.write(data_dict)
+    
+
+# if 만약 입력된 정보가 틀리다면 Error msg를 띄운다.
+else :
+    with st.spinner('Wait for it'):
+        time.sleep(5)
+    
+    st.error('Error! please write again')
+
+
+
